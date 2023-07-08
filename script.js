854003c42321994d8fee733e47e816f636596565
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-07-08 15:55:31 +0530

    feat: add visual transition

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function playSound(e) {
   
   const audio = document.querySelector(`audio[data-key=${e.code}]`);
+  const key = document.querySelector(`.key[data-key=${e.code}]`);
 
   if (!audio) return;
 
   audio.currentTime = 0;
   audio.play();
+  key.classList.add('playing');
 }
+
 
 window.addEventListener("keydown", playSound);
