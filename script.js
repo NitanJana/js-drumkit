56e56fd203fc39b5621f2a4ec9c0de973840644f
Nitan Jana <67586363+NitanJana@users.noreply.github.com>
2023-07-08 19:40:51 +0530

    feat: add functionality for touch-enabled devices

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function playSound(e) {
+function playSound(keyValue) {
   //audio with the same data attribute as the code property of key pressed
-  const audio = document.querySelector(`audio[data-key=${e.code}]`);
-  const key = document.querySelector(`.key[data-key=${e.code}]`);
+  
+  const audio = document.querySelector(`audio[data-key="${keyValue}"]`);
+  const key = document.querySelector(`.key[data-key="${keyValue}"]`);
 
   //to ignore other keypress than intended
-  if (!audio) return;
+  if (!audio || !key) return;
 
   // to play sound in rapid succession
   audio.currentTime = 0;
@@ -12,6 +13,22 @@ function playSound(e) {
   audio.play();
   // to add visual transition to the key playing
   key.classList.add('playing');
+}
+
+function playSoundKeyBoard(e) {
+  playSound(e.code);
+}
+function playSoundTouchScreen(e) {
+  let target = e.target;
+  // Traverse up the DOM tree until a .key element is found
+  //it should play sound even when touching child nodes(texts) of the div
+  while (target && !target.classList.contains('key')) {
+    target = target.parentNode;
+  }
+  if (!target) return;
+
+  const keyValue = target.getAttribute('data-key');
+  playSound(keyValue);
 }
 
 function removeTransition(e) {
@@ -25,4 +42,8 @@ const keys = document.querySelectorAll('.key');
 // remove transition after playing is done
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener("keydown", playSound);
+// For touch-enabled devices
+keys.forEach(key => key.addEventListener('touchstart',playSoundTouchScreen))
+
+// For keyboard enabled devices
+window.addEventListener("keydown", playSoundKeyBoard);
